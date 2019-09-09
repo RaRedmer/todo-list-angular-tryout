497a15955103d560a6f0f5494f050a7a56c7bcf3
Rafamazing <ra.redmer@outlook.com>
2019-09-09 22:59:09 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Raphael´s To-Do List';
+
+  constructor(){
+    // console.log("123")
+  }
+  
+}
